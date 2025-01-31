67ce57247d377c27f4b5c672a86a6ce688d22b3b
Ulzhan <kozqarashigi@gmail.com>
2025-01-31 22:59:45 +0500

    Modified smart contract and updated test cases

--- contracts/UniversityToken.sol
+++ contracts/UniversityToken.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract UniversityToken is ERC20 {
-    constructor() ERC20("Ulzhan_Nurgul", "UN") {
-        _mint(msg.sender, 2000 * 10**decimals());
+    address public owner;
+
+    constructor(address _owner) ERC20("UniversityToken", "UT") {
+        owner = _owner;
+        _mint(_owner, 1000 * 10 ** decimals()); // Mint initial supply to owner
     }
 }
