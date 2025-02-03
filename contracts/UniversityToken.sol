cfcee33db04341ff0daecba14c7b788978eb5c47
ulzhan <ulzhanermekkyzy@gmail.com>
2025-02-03 14:33:26 +0500

    Update UniversityToken.sol

--- contracts/UniversityToken.sol
+++ contracts/UniversityToken.sol
@@ -1,13 +1,37 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract UniversityToken is ERC20 {
-    address public owner;
+    address public lastSender;
+    address public lastReceiver;
+    uint256 public lastTimestamp;
 
-    constructor(address _owner) ERC20("UniversityToken", "UT") {
-        owner = _owner;
-        _mint(_owner, 1000 * 10 ** decimals()); // Mint initial supply to owner
+    constructor() ERC20("Ulzhan_Nurgul", "UN") {
+    _mint(msg.sender, 2000 * 10**decimals());
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        lastSender = msg.sender;
+        lastReceiver = recipient;
+        lastTimestamp = block.timestamp;
+        return super.transfer(recipient, amount);
+    }
+
+    function getLastTransactionTimestamp() public view returns (string memory) {
+        return _formatTimestamp(lastTimestamp);
+    }
+
+    function getLastSender() public view returns (address) {
+        return lastSender;
+    }
+
+    function getLastReceiver() public view returns (address) {
+        return lastReceiver;
+    }
+
+    function _formatTimestamp(uint256 timestamp) internal pure returns (string memory) {
+        return string(abi.encodePacked(timestamp));
     }
 }
