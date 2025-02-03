edc28af77bfdbcde6e57947ed0240ece0b2d2b42
ulzhan <ulzhanermekkyzy@gmail.com>
2025-02-03 22:13:30 +0500

    Update UniversityToken.sol

--- contracts/UniversityToken.sol
+++ contracts/UniversityToken.sol
@@ -9,7 +9,7 @@ contract UniversityToken is ERC20 {
     uint256 public lastTimestamp;
 
     constructor() ERC20("Ulzhan_Nurgul", "UN") {
-    _mint(msg.sender, 2000 * 10**decimals());
+        _mint(msg.sender, 2000 * 10**decimals());
     }
 
     function transfer(address recipient, uint256 amount) public override returns (bool) {
@@ -32,6 +32,34 @@ contract UniversityToken is ERC20 {
     }
 
     function _formatTimestamp(uint256 timestamp) internal pure returns (string memory) {
-        return string(abi.encodePacked(timestamp));
+        uint256 SECONDS_PER_DAY = 86400;
+        uint16 ORIGIN_YEAR = 1970;
+
+        uint256 daysSinceEpoch = timestamp / SECONDS_PER_DAY;
+        uint16 year = ORIGIN_YEAR;
+        uint256 leapYears = (daysSinceEpoch + 1) / 1461; // Approximate leap year count
+        year += uint16((daysSinceEpoch - leapYears) / 365);
+
+        return uintToString(year);
+    }
+
+    //  Define uintToString Function
+    function uintToString(uint256 value) internal pure returns (string memory) {
+        if (value == 0) {
+            return "0";
+        }
+        uint256 temp = value;
+        uint256 digits;
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        while (value != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+            value /= 10;
+        }
+        return string(buffer);
     }
 }
